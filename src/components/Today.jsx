f6cbe13eb366fab12ca7a18fc1dab6ee2248a8e3
RauboLuk <rauboluk@gmail.com>
2021-05-22 01:49:41 +0200

    fix: font-weight for today

--- src/components/Today.jsx
+++ src/components/Today.jsx
@@ -5,7 +5,7 @@ import background from "../assets/images/Cloud-background.png";
 
 const Today = () => {
   return (
-    <div className="container bg-navyBlue-light pb-24">
+    <div className="bg-navyBlue-light pb-24">
       <header className="flex justify-between px-3 pt-4">
         <button className="bg-gray-dark text-gray-light px-4 max-h-10 text-base">
           Seach for places
@@ -24,7 +24,9 @@ const Today = () => {
       <p className="m-auto -mt-12 w-max text-10xl text-gray-light">
         15<span className="m-auto text-5xl text-gray-text1 font-thin">℃</span>
       </p>
-      <div className="m-auto mt-0 w-max text-gray-text1 text-4xl">Shower</div>
+      <div className="m-auto mt-0 w-max text-gray-text1 text-4xl font-semibold">
+        Shower
+      </div>
       <div className="m-auto mt-12 w-max flex gap-4 text-gray-text1">
         <div>Today</div>
         <div>•</div>
@@ -32,7 +34,7 @@ const Today = () => {
       </div>
       <div className="m-auto mt-8 w-max flex gap-4 text-gray-text1">
         <LocationOnIcon />
-        <p>Helsinki</p>
+        <p className="font-semibold">Helsinki</p>
       </div>
     </div>
   );
